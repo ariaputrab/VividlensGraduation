b097ad8944fd28823a07db9d8c93a875be651b4b
ariaputrab <114902342+ariaputrab@users.noreply.github.com>
2026-07-11 20:10:27 +0700

    Add files via upload

---
+++ pengaturan.js
@@ -0,0 +1,13 @@
+const CONFIG_APP = {
+    // 1. URL Supabase Anda (Bisa dilihat di Project Settings -> API)
+    supabaseUrl: "https://xyz-id-database-kamu.supabase.co",
+    
+    // 2. Anon Key Supabase Anda (Bisa dilihat di Project Settings -> API)
+    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNreXBlY...",
+    
+    // 3. API Key Google Cloud Console Anda (Untuk membaca folder Google Drive)
+    googleApiKey: "AIzaSyA123-Kunci-Google-Drive-Kamu-Yang-Aktif",
+    
+    // 4. Nama Vendor Studio Foto Anda
+    namaVendor: "VIVIDLENS"
+};
