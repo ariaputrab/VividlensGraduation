cbf861036f6616a09d8b923966df7e3914b19c6c
ariaputrab <114902342+ariaputrab@users.noreply.github.com>
2026-07-11 20:36:56 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -1,11 +1,11 @@
 const CONFIG_APP = {
-    // URL ini otomatis didapatkan dari kode token kamu di atas
+    // 1. URL ini sudah benar 100% hasil ekstrak dari token kamu
     supabaseUrl: "https://aizhweokpxfrczqsuikr.supabase.co",
     
-    // Tempelkan token panjang milikmu tadi di sini
+    // 2. Token ini juga sudah benar dan sangat lengkap
     supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpemh3ZW9reHB4ZnJjenFzdWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3NTYwNTAsImV4cCI6MjA5OTMzMjA1MH0.ceD7xZpYpB1VIGXliS8r6huCZ7RNrIPh6vxN2LtulB8",
     
-    // Isi dengan Google API Key kamu untuk Drive
+    // 3. Pastikan bagian ini diisi dengan API Key Google Drive kamu (jika pakai)
     googleApiKey: "AIzaSyA123-Kunci-Google-Drive-Kamu",
     
     namaVendor: "VIVIDLENS"
